4796578f088fcbb2c45171cd8534e1a25bb4f640
Phil Fernandez <norcal618@gmail.com>
2021-01-17 15:56:35 -0800

    first commit

---
+++ src/main/java/com/hcl/taskmanager/model/Task.java
@@ -0,0 +1,33 @@
+package com.hcl.taskmanager.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@Entity
+public class Task {
+    private @Id @GeneratedValue Long id;
+    private String taskName;
+    private String startDate;
+    private String endDate;
+    private String description;
+    private String severity;
+
+    public Task(String taskName, String startDate, String endDate, String description,
+            String severity) {
+        this.taskName = taskName;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.description = description;
+        this.severity = severity;
+    }
+    
+}
