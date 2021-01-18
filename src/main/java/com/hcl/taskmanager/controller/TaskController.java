968a88dbc88a88ad1580427ab73776dc1df6f47f
Phil Fernandez <norcal618@gmail.com>
2021-01-17 22:24:55 -0800

    able to add task

--- src/main/java/com/hcl/taskmanager/controller/TaskController.java
+++ src/main/java/com/hcl/taskmanager/controller/TaskController.java
@@ -4,8 +4,11 @@ import com.hcl.taskmanager.data.service.TaskService;
 import com.hcl.taskmanager.model.Task;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.PostMapping;
+
 
 @Controller
 @RequestMapping("/task")
@@ -20,4 +23,11 @@ public class TaskController {
     public ModelAndView newTask() {
         return new ModelAndView("newTask", "task", new Task());
     }
+
+    @PostMapping("/create")
+    public String saveNewTask(@ModelAttribute Task task) {
+        taskService.createTask(task);
+        return "tasks";
+    }
+    
 }
---
+++ src/main/webapp/WEB-INF/view/tasks.jsp
@@ -0,0 +1,13 @@
+<%@taglib uri="http://www.springframework.org/tags/form" prefix="form"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Task List</title>
+</head>
+<body>
+    All tasks shown!
+</body>
+</html>
