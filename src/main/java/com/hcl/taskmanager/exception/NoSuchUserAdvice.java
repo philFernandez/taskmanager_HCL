b69397c3fc46e2f1a18347f8dd0951d7c668be31
Phil Fernandez <norcal618@gmail.com>
2021-01-19 00:00:03 -0800

    login views seem good

--- src/main/java/com/hcl/taskmanager/exception/NoSuchUserAdvice.java
+++ src/main/java/com/hcl/taskmanager/exception/NoSuchUserAdvice.java
@@ -1,18 +1,17 @@
 package com.hcl.taskmanager.exception;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
 
 @ControllerAdvice
 public class NoSuchUserAdvice {
 
-    @ResponseBody
     @ExceptionHandler(NoSuchUserException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public String noSuchUserFoundAdvice(NoSuchUserException e) {
-        return e.getMessage();
+    public ModelAndView noSuchUserFoundAdvice(NoSuchUserException e) {
+        return new ModelAndView("noSuchUser", "noUserException", e.getMessage());
     }
-    
 }
